120a22d40c589ec9fe106f43f6782216313ea885
AndresPonciano <andres.ponciano123@gmail.com>
2020-02-08 16:29:28 -0500

    added login with twitter

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { auth } from 'firebase';
+ 
+// const provider = new auth.TwitterAuthProvider();
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(public afAuth: AngularFireAuth) { }
+
+  doTwitterLogin() {
+    console.log('pee');
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.TwitterAuthProvider();
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => {
+        resolve(res);
+      }, err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
